a50444cf7d95242bced0e280bc465e548dd3a6ba
jhgbrt <jeroen@haegebaert.com>
2023-01-13 20:22:34 +0100

    support static methods

--- src/Net.Code.AdventOfCode.Toolkit/Logic/AoCRunner.cs
+++ src/Net.Code.AdventOfCode.Toolkit/Logic/AoCRunner.cs
@@ -5,7 +5,9 @@ using Net.Code.AdventOfCode.Toolkit.Core;
 
 using System.Diagnostics;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 namespace Net.Code.AdventOfCode.Toolkit.Logic;
 
@@ -38,7 +40,7 @@ class AoCRunner : IAoCRunner
         var t2 = day < 25
             ? Run(() => aoc.Part2())
             : new Result(ResultStatus.Ok, "", TimeSpan.Zero);
-        logger.LogDebug($"{year}/{day}, Part 2: result = {t1.Value} - {t1.Elapsed}");
+        logger.LogDebug($"{year}/{day}, Part 2: result = {t2.Value} - {t2.Elapsed}");
         progress(2, t2);
 
         var result = new DayResult(year, day, t1, t2);
@@ -47,48 +49,56 @@ class AoCRunner : IAoCRunner
 
         return result;
     }
-
+    bool IsCompilerGenerated(Type type) => type.GetCustomAttribute<CompilerGeneratedAttribute>() != null;
     private dynamic? GetAoC(string? typeName, int year, int day)
     {
         var assembly = resolver.GetEntryAssembly();
         if (assembly == null) throw new Exception("no entry assembly?");
 
+        var regex = new Regex(@$"[^\\d]{year}.*[^\\d]*{day:00}", RegexOptions.Compiled);
+
         Type? type = null;
+        MethodInfo? part1 = null;
+        MethodInfo? part2 = null;
         if (string.IsNullOrEmpty(typeName))
         {
             foreach (var t in assembly.GetTypes().OrderBy(t => t.Name))
             {
-                if (t.Name.Contains('<')) // skip compiler-generated classes
-                    continue;
-
-                var name = t.FullName ?? t.Name;
-                if (t.Namespace is null || !t.Namespace.Contains($"{year}") || !t.Namespace.Replace($"{year}", "").ToLower().Contains($"day{day:00}"))
+                if (IsCompilerGenerated(t)) // skip compiler-generated classes
                 {
                     continue;
                 }
 
                 logger.LogDebug($"considered {t.FullName}");
+
+                if (!regex.IsMatch(t.FullName ?? t.Name))
+                {
+                    continue;
+                }
+
                 var methods = t.GetMethods();
-                if (!methods.Any(m => m.Name == "Part1" && m.GetParameters().Length == 0))
+                var method1 = methods.FirstOrDefault(m => m.Name is "Part1" && m.GetParameters().Length is 0);
+                var method2 = methods.FirstOrDefault(m => m.Name is "Part2" && m.GetParameters().Length is 0);
+
+                if (method1 is null || method2 is null)
                 {
-                    logger.LogDebug($"{t.Name} does not have a parameterless method Part1()");
+                    logger.LogDebug($"{t.Name} does not have two parameterless methods Part1() and Part2()");
                     continue;
                 }
-                if (!methods.Any(m => m.Name == "Part2" && m.GetParameters().Length == 0))
-                {
-                    logger.LogDebug($"{t.Name} does not have a parameterless method Part2()");
-                    continue;
-                }
-                type = t;
+
+                (type, part1, part2) = (t, method1, method2);
                 break;
             }
         }
         else
         {
             type = assembly.GetType(string.Format(typeName, year, day));
+            var methods = type?.GetMethods();
+            part1 = methods?.FirstOrDefault(m => m.Name == "Part1" && m.GetParameters().Length == 0);
+            part2 = methods?.FirstOrDefault(m => m.Name == "Part2" && m.GetParameters().Length == 0);
         }
 
-        if (type is null)
+        if (type is null || part1 is null || part2 is null)
         {
             logger.LogWarning($"No AoC implementation found for {year}/{day}");
             return null;
@@ -96,9 +106,24 @@ class AoCRunner : IAoCRunner
 
         logger.LogInformation($"Using implementation type: {type}");
 
-        return Activator.CreateInstance(type);
+        if (type.IsAbstract)
+            return new Runner(part1, part2);
+        else
+            return Activator.CreateInstance(type);
     }
-
+    class Runner
+    {
+        readonly Func<object> part1;
+        readonly Func<object> part2;
+        public Runner(MethodInfo part1, MethodInfo part2)
+        {
+            this.part1 = part1.CreateDelegate<Func<object>>();
+            this.part2 = part2.CreateDelegate<Func<object>>();
+        }
+        public object Part1() => part1();
+        public object Part2() => part2();
+    }
+   
     static Result Run(Func<object> f)
     {
         var sw = Stopwatch.StartNew();
