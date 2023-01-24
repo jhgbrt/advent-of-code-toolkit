d0bd470321a82b6e330e3c12a09e7d693272ddce
jhgbrt <jeroen@haegebaert.com>
2023-01-24 23:04:18 +0100

    fix!

--- src/Net.Code.AdventOfCode.Toolkit/Commands/Migrate.cs
+++ src/Net.Code.AdventOfCode.Toolkit/Commands/Migrate.cs
@@ -1,35 +1,20 @@
-<<<<<<< HEAD
-﻿
+
 using Net.Code.AdventOfCode.Toolkit.Core;
 using Net.Code.AdventOfCode.Toolkit.Data;
 
 using Spectre.Console.Cli;
 
-using System.Text.Json;
-
-using Spectre.Console.Cli;
-
-=======
-﻿using Net.Code.AdventOfCode.Toolkit.Core;
-
-using Spectre.Console.Cli;
-
->>>>>>> 780ecea (remove cache)
 namespace Net.Code.AdventOfCode.Toolkit.Commands;
 
 class Migrate : AsyncCommand<Migrate.Settings>
 {
     public class Settings : CommandSettings { }
     private readonly IAoCDbContext dbcontext;
-<<<<<<< HEAD
-=======
-
->>>>>>> 780ecea (remove cache)
     public Migrate(IAoCDbContext dbcontext)
     {
         this.dbcontext = dbcontext;
     }
-    public override Task<int> ExecuteAsync(CommandContext context, Settings settings)
+    public async override Task<int> ExecuteAsync(CommandContext context, Settings settings)
     {
         dbcontext.Migrate();
         return Task.FromResult(0);
